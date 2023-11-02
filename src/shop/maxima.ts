ced1f8d19a17eca3638f24877f0a2dadb03a98e3
Mihails Dudarevs <deadcatofdoom@googlemail.com>
2023-11-02 17:02:43 +0200

    shop error handling

--- src/shop/maxima.ts
+++ src/shop/maxima.ts
@@ -1,5 +1,5 @@
 import { Logger } from '@nestjs/common';
-import puppeteer from 'puppeteer';
+import puppeteer, { Browser } from 'puppeteer';
 import { RawProductDto } from 'src/dto/products.dto';
 import { BaseScraper } from 'src/scrapperFactory/base-scrapper';
 
@@ -7,73 +7,85 @@ export class Maxima extends BaseScraper {
   private readonly logger = new Logger('Maxima');
 
   async scrapeProducts(query: string, page: number): Promise<RawProductDto[]> {
-    const browser = await puppeteer.launch({
-      headless: 'new',
-    });
-    const webPage = await browser.newPage();
-    const url = `https://barbora.lv/meklet?q=${query}&page=${page}`;
-    this.logger.log('Scrapping from ' + url);
-    await webPage.goto(url);
+    let browser: Browser;
+    try {
+      const browser = await puppeteer.launch({
+        headless: 'new',
+      });
+      const webPage = await browser.newPage();
+      const url = `https://barbora.lv/meklet?q=${query}&page=${page}`;
+      this.logger.log('Scrapping from ' + url);
+      await webPage.goto(url);
 
-    const products = await webPage.$$eval(
-      '.tw-flex-shrink-0.tw-list-none.tw-w-full',
-      (items): RawProductDto[] => {
-        return items.map((item): RawProductDto => {
-          const name =
-            (
-              item.querySelector(
-                'a.tw-break-words.tw-text-b-paragraph-sm',
-              ) as HTMLElement
-            )?.textContent?.trim() || '';
+      const products = await webPage.$$eval(
+        '.tw-flex-shrink-0.tw-list-none.tw-w-full',
+        (items): RawProductDto[] => {
+          return items.map((item): RawProductDto => {
+            const name =
+              (
+                item.querySelector(
+                  'a.tw-break-words.tw-text-b-paragraph-sm',
+                ) as HTMLElement
+              )?.textContent?.trim() || '';
 
-          const imgElement = item.querySelector('img') as HTMLImageElement;
-          const imgURL = imgElement?.src || '';
-          const originalPriceElement = item.querySelector(
-            '.tw-pl-2.tw-text-b-paragraph-xs.tw-font-bold.tw-text-gray-400.lg\\:tw-text-b-paragraph-sm',
-          );
-          const originalPrice =
-            originalPriceElement?.textContent?.trim().replace(',', '.') || '';
+            const imgElement = item.querySelector('img') as HTMLImageElement;
+            const imgURL = imgElement?.src || '';
+            const originalPriceElement = item.querySelector(
+              '.tw-pl-2.tw-text-b-paragraph-xs.tw-font-bold.tw-text-gray-400.lg\\:tw-text-b-paragraph-sm',
+            );
+            const originalPrice =
+              originalPriceElement?.textContent?.trim().replace(',', '.') || '';
 
-          const discountPricePart1 =
-            (
-              item.querySelector(
-                '.tw-mr-0\\.5.tw-text-b-price-sm.tw-font-semibold.lg\\:tw-text-b-price-xl',
-              ) as HTMLElement
-            )?.textContent?.trim() || '0';
+            const discountPricePart1 =
+              (
+                item.querySelector(
+                  '.tw-mr-0\\.5.tw-text-b-price-sm.tw-font-semibold.lg\\:tw-text-b-price-xl',
+                ) as HTMLElement
+              )?.textContent?.trim() || '0';
 
-          const discountPricePart2 =
-            (
-              item.querySelector(
-                '.tw-text-b-price-xs.tw-font-semibold.lg\\:tw-text-b-price-lg',
-              ) as HTMLElement
-            )?.textContent?.trim() || '00';
+            const discountPricePart2 =
+              (
+                item.querySelector(
+                  '.tw-text-b-price-xs.tw-font-semibold.lg\\:tw-text-b-price-lg',
+                ) as HTMLElement
+              )?.textContent?.trim() || '00';
 
-          const discountedPrice =
-            discountPricePart1 + '.' + discountPricePart2 + '€';
+            const discountedPrice =
+              discountPricePart1 + '.' + discountPricePart2 + '€';
 
-          const pricePerUnitElement = item.querySelector(
-            '.tw-flex.tw-flex-row.tw-justify-between > span',
-          );
-          const pricePerUnit =
-            pricePerUnitElement?.textContent?.replace(',', '.') || '';
-          const cardOwnerOnly = !!item.querySelector(
-            'span.tw-mr-1.tw-text-b-price-3xs.tw-font-semibold',
-          );
-          return {
-            id: 2,
-            name,
-            imgURL,
-            originalPrice,
-            discountedPrice,
-            pricePerUnit,
-            cardOwnerOnly,
-          };
-        });
-      },
-    );
+            const pricePerUnitElement = item.querySelector(
+              '.tw-flex.tw-flex-row.tw-justify-between > span',
+            );
+            const pricePerUnit =
+              pricePerUnitElement?.textContent?.replace(',', '.') || '';
+            const cardOwnerOnly = !!item.querySelector(
+              'span.tw-mr-1.tw-text-b-price-3xs.tw-font-semibold',
+            );
+            return {
+              id: 2,
+              name,
+              imgURL,
+              originalPrice,
+              discountedPrice,
+              pricePerUnit,
+              cardOwnerOnly,
+            };
+          });
+        },
+      );
 
-    await browser.close();
-    this.sanitizeProducts(products);
-    return products;
+      await browser.close();
+      this.sanitizeProducts(products);
+      return products;
+    } catch (error) {
+      this.logger.error('An error occurred during scraping', error);
+      throw new Error(
+        'Scraping failed. Please check the logs for more details.',
+      );
+    } finally {
+      if (browser) {
+        await browser.close();
+      }
+    }
   }
 }
--- src/shop/rimi.ts
+++ src/shop/rimi.ts
@@ -1,5 +1,5 @@
 import { Logger } from '@nestjs/common';
-import puppeteer from 'puppeteer';
+import puppeteer, { Browser } from 'puppeteer';
 import { RawProductDto } from 'src/dto/products.dto';
 import { BaseScraper } from 'src/scrapperFactory/base-scrapper';
 
@@ -7,106 +7,118 @@ export class Rimi extends BaseScraper {
   private readonly logger = new Logger('Rimi');
 
   async scrapeProducts(query: string, page: number): Promise<RawProductDto[]> {
-    const browser = await puppeteer.launch({
-      headless: 'new',
-    });
-    const webPage = await browser.newPage();
-    const url = `https://www.rimi.lv/e-veikals/lv/meklesana?page=${page}&pageSize=52&query=${query}%3Arelevance%3AassortmentStatus%3AinAssortment`;
-    this.logger.log('Scrapping from ' + url);
-    await webPage.goto(url);
+    let browser: Browser;
+    try {
+      const browser = await puppeteer.launch({
+        headless: 'new',
+      });
+      const webPage = await browser.newPage();
+      const url = `https://www.rimi.lv/e-veikals/lv/meklesana?page=${page}&pageSize=52&query=${query}%3Arelevance%3AassortmentStatus%3AinAssortment`;
+      this.logger.log('Scrapping from ' + url);
+      await webPage.goto(url);
 
-    const products = await webPage.$$eval(
-      '.product-grid__item',
-      (items): RawProductDto[] => {
-        return items.map((item): RawProductDto => {
-          const name =
-            (
-              item.querySelector('.card__name') as HTMLElement
-            )?.textContent?.trim() || '';
-          const imgElement = item.querySelector(
-            '.card__image-wrapper img',
-          ) as HTMLImageElement;
-          const imgURL = imgElement?.src || '';
-
-          let originalPrice = null;
-          let discountedPrice = null;
-
-          if (item.querySelector('.old-price-tag.card__old-price')) {
-            originalPrice =
+      const products = await webPage.$$eval(
+        '.product-grid__item',
+        (items): RawProductDto[] => {
+          return items.map((item): RawProductDto => {
+            const name =
               (
-                item.querySelector(
-                  '.old-price-tag.card__old-price span',
-                ) as HTMLElement
-              )?.textContent?.replace(/\s+/g, '') || '';
-            originalPrice = originalPrice
-              .replace(/(\d)(\d{2}€)/, '$1.$2')
-              .replace('/gab.', '');
+                item.querySelector('.card__name') as HTMLElement
+              )?.textContent?.trim() || '';
+            const imgElement = item.querySelector(
+              '.card__image-wrapper img',
+            ) as HTMLImageElement;
+            const imgURL = imgElement?.src || '';
 
-            discountedPrice =
-              (
-                item.querySelector('.price-tag.card__price') as HTMLElement
-              )?.textContent
-                ?.replace(/\s+/g, '')
-                .replace(/(\d)(\d{2}€)/, '$1.$2') || null;
-            discountedPrice = discountedPrice.replace('/gab.', '');
-          } else if (item.querySelector('.price-badge__body')) {
-            // For cases when the discount is for cardholders
-            discountedPrice = Array.from(
-              item.querySelectorAll(
-                '.price-badge__body .price-badge__price span',
-              ),
-            )
-              .map((el) => el.textContent)
-              .join('.')
-              .concat('€');
+            let originalPrice = null;
+            let discountedPrice = null;
 
-            originalPrice =
-              (
-                item.querySelector('.price-tag.card__price') as HTMLElement
-              )?.textContent?.replace(/\s+/g, '') || '';
-            originalPrice = originalPrice
-              .replace(/(\d)(\d{2}€)/, '$1.$2')
-              .replace('/gab.', '');
-          } else {
-            originalPrice =
-              (
-                item.querySelector('.price-tag.card__price') as HTMLElement
-              )?.textContent?.replace(/\s+/g, '') || '';
-            originalPrice = originalPrice
-              .replace(/(\d)(\d{2}€)/, '$1.$2')
-              .replace('/gab.', '');
-          }
+            if (item.querySelector('.old-price-tag.card__old-price')) {
+              originalPrice =
+                (
+                  item.querySelector(
+                    '.old-price-tag.card__old-price span',
+                  ) as HTMLElement
+                )?.textContent?.replace(/\s+/g, '') || '';
+              originalPrice = originalPrice
+                .replace(/(\d)(\d{2}€)/, '$1.$2')
+                .replace('/gab.', '');
 
-          // Remove any extra characters from the prices
-          discountedPrice = discountedPrice?.replace('.€€', '€');
+              discountedPrice =
+                (
+                  item.querySelector('.price-tag.card__price') as HTMLElement
+                )?.textContent
+                  ?.replace(/\s+/g, '')
+                  .replace(/(\d)(\d{2}€)/, '$1.$2') || null;
+              discountedPrice = discountedPrice.replace('/gab.', '');
+            } else if (item.querySelector('.price-badge__body')) {
+              // For cases when the discount is for cardholders
+              discountedPrice = Array.from(
+                item.querySelectorAll(
+                  '.price-badge__body .price-badge__price span',
+                ),
+              )
+                .map((el) => el.textContent)
+                .join('.')
+                .concat('€');
 
-          let pricePerUnit =
-            (item.querySelector('.card__price-per') as HTMLElement)
-              ?.textContent || '';
-          pricePerUnit = pricePerUnit
-            .replace(/\s+/g, '')
-            .trim()
-            .replace(',', '.');
+              originalPrice =
+                (
+                  item.querySelector('.price-tag.card__price') as HTMLElement
+                )?.textContent?.replace(/\s+/g, '') || '';
+              originalPrice = originalPrice
+                .replace(/(\d)(\d{2}€)/, '$1.$2')
+                .replace('/gab.', '');
+            } else {
+              originalPrice =
+                (
+                  item.querySelector('.price-tag.card__price') as HTMLElement
+                )?.textContent?.replace(/\s+/g, '') || '';
+              originalPrice = originalPrice
+                .replace(/(\d)(\d{2}€)/, '$1.$2')
+                .replace('/gab.', '');
+            }
 
-          const cardOwnerOnly = !!item.querySelector('div.price-badge__body');
+            // Remove any extra characters from the prices
+            discountedPrice = discountedPrice?.replace('.€€', '€');
 
-          return {
-            id: 1,
-            name,
-            imgURL,
-            originalPrice,
-            discountedPrice,
-            pricePerUnit,
-            cardOwnerOnly,
-          };
-        });
-      },
-    );
+            let pricePerUnit =
+              (item.querySelector('.card__price-per') as HTMLElement)
+                ?.textContent || '';
+            pricePerUnit = pricePerUnit
+              .replace(/\s+/g, '')
+              .trim()
+              .replace(',', '.');
 
-    await browser.close();
+            const cardOwnerOnly = !!item.querySelector('div.price-badge__body');
 
-    products.map((item) => this.addDiscount(item));
+            return {
+              id: 1,
+              name,
+              imgURL,
+              originalPrice,
+              discountedPrice,
+              pricePerUnit,
+              cardOwnerOnly,
+            };
+          });
+        },
+      );
 
-    return products;
+      await browser.close();
+
+      products.map((item) => this.addDiscount(item));
+
+      return products;
+    } catch (error) {
+      this.logger.error('An error occurred during scraping', error);
+      throw new Error(
+        'Scraping failed. Please check the logs for more details.',
+      );
+    } finally {
+      if (browser) {
+        await browser.close();
+      }
+    }
   }
 }
