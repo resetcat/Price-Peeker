4d20821abfb18110cf972a89b7591f1fc582998d
Mihails Dudarevs <deadcatofdoom@googlemail.com>
2023-11-06 17:43:02 +0200

    factory  logger

--- src/scrapperFactory/scrapper-factory.ts
+++ src/scrapperFactory/scrapper-factory.ts
@@ -1,8 +1,11 @@
 import { Rimi } from 'src/shop/rimi';
 import { Maxima } from 'src/shop/maxima';
 import { StoreScraper } from './store-scrapper.interface';
+import { Logger } from '@nestjs/common';
 
 export class StoreScraperFactory {
+  private static readonly logger = new Logger(StoreScraperFactory.name);
+
   static createScraper(storeName: string): StoreScraper {
     switch (storeName) {
       case 'R-Gshop':
@@ -11,6 +14,7 @@ export class StoreScraperFactory {
         return new Maxima();
       // ... add more cases as new stores are added
       default:
+        this.logger.error(`Unknown store name: ${storeName}`);
         throw new Error(`Scraper for store ${storeName} not found.`);
     }
   }
